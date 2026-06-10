07f4ca1d33bb8688cfeab641af0970c33d2bd47e
meghadvaniit23d1-star <meghadvani.it23d1@scet.ac.in>
2026-06-10 13:23:50 +0530

    Add FCM token saving on login

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./nexora-6988-firebase-adminsdk-fbsvc-1116c56090.json');
+const serviceAccount = require('./serviceAccountKey.json');
 
 if (!admin.apps.length) {
   admin.initializeApp({
