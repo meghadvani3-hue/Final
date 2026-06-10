f8a5cbc86ee25f77faf94b9b3c4c5e4956d9f1d2
meghadvaniit23d1-star <meghadvani.it23d1@scet.ac.in>
2026-06-10 13:38:16 +0530

    Updatessssss

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,4 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./nexora-6988-firebase-adminsdk-fbsvc-1116c56090.json');
 
 if (!admin.apps.length) {
   admin.initializeApp({
